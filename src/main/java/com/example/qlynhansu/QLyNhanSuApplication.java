0cd89e364b57a7a57a5230df15defe6606eef468
DucAnhPTITD19CN04 <66071743+ducanhptit0312@users.noreply.github.com>
2022-05-27 23:22:00 +0700

    fix session next

--- src/main/java/com/example/qlynhansu/QLyNhanSuApplication.java
+++ src/main/java/com/example/qlynhansu/QLyNhanSuApplication.java
@@ -1,5 +1,6 @@
 package com.example.qlynhansu;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,5 +11,4 @@ public class QLyNhanSuApplication {
     public static void main(String[] args) {
         SpringApplication.run(QLyNhanSuApplication.class, args);
     }
-
 }
